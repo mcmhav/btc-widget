ab373f8d33492a5abd8a354ff1130f165eef23a8
mcmhav <mcmhav@gmail.com>
2018-08-09 00:01:42 +0200

    asfd

---
+++ app/src/main/java/com/mcmhav/btcwidget/Settings.java
@@ -0,0 +1,162 @@
+package com.mcmhav.btcwidget;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Settings {
+  private SharedPreferences mPreferences;
+  private boolean mMonitorWatch;
+
+  public static final String PREF_PREFIX = "com.mcmhav.btcwidget.";
+
+  private Settings() {}
+
+  public boolean monitorWatch(int[] appWidgetIds) {
+    if (appWidgetIds == null) {
+      return mMonitorWatch;
+    }
+
+    boolean monitor = false;
+    for (int appWidgetId : appWidgetIds) {
+      GraphSettings gs = getGraphSettings(appWidgetId);
+      if (gs.showWatchGraph()) {
+        monitor = true;
+        break;
+      }
+    }
+    if (mMonitorWatch != monitor) {
+      mMonitorWatch = monitor;
+      mPreferences.edit().putBoolean(PREF_PREFIX + "MonitorWatch", monitor).apply();
+    }
+    return mMonitorWatch;
+  }
+
+  public GraphSettings getGraphSettings(int appWidgetId) {
+    String prefix = String.format("%s(%d).", PREF_PREFIX, appWidgetId);
+    if (!mPreferences.contains(prefix + "AutoGraph")) {
+      // if we don't have settings for this graph, copy over the 'default' preferences.
+      GraphSettings gs = GraphSettings.get(mPreferences, PREF_PREFIX);
+      gs.save(mPreferences, prefix);
+    }
+    return GraphSettings.get(mPreferences, String.format("%s(%d).", PREF_PREFIX, appWidgetId));
+  }
+
+  public static Settings get(Context context) {
+    SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+
+    Settings s = new Settings();
+    s.mPreferences = pref;
+    s.mMonitorWatch = pref.getBoolean(PREF_PREFIX+"MonitorWatch", true);
+    return s;
+  }
+
+  public void save(Context context) {
+    SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+    pref.edit()
+      .putBoolean(PREF_PREFIX + "MonitorWatch", mMonitorWatch)
+      .apply();
+  }
+
+  public static class GraphSettings {
+    private String mPrefix;
+    private int mGraphWidth;
+    private int mGraphHeight;
+    private boolean mShowWatchGraph;
+    private boolean mShowBatteryGraph;
+    private boolean mShowBatteryCurrentInstant;
+    private boolean mSmoothBatteryCurrentInstant;
+    private boolean mInvertBatteryCurrentInstant;
+    private boolean mShowBatteryCurrentAvg;
+    private boolean mShowBatteryEnergy;
+    private boolean mShowTempGraph;
+    private boolean mSmoothTemp;
+    private boolean mTempCelsius;
+    private int mNumHours;
+    private boolean mShowTimeScale;
+    private boolean mShowTimeLines;
+    private boolean mAutoGraphSize;
+
+    public boolean autoGraphSize() { return mAutoGraphSize; }
+    public int getGraphWidth() {
+      return mGraphWidth;
+    }
+    public void setGraphWidth(int width) {
+      mGraphWidth = width;
+    }
+    public int getGraphHeight() {
+      return mGraphHeight;
+    }
+    public void setGraphHeight(int height) {
+      mGraphHeight = height;
+    }
+    public boolean showWatchGraph() { return mShowWatchGraph; }
+    public boolean showBatteryGraph() { return mShowBatteryGraph; }
+    public boolean showBatteryCurrentInstant() { return mShowBatteryCurrentInstant; }
+    public boolean smoothBatteryCurrentInstant() { return mSmoothBatteryCurrentInstant; }
+    public boolean invertBatteryCurrentInstant() { return mInvertBatteryCurrentInstant; }
+    public boolean showBatteryCurrentAvg() { return mShowBatteryCurrentAvg; }
+    public boolean showBatteryEnergy() { return mShowBatteryEnergy; }
+    public boolean showTemperatureGraph() {
+      return mShowTempGraph;
+    }
+    public boolean tempInCelsius() { return mTempCelsius; }
+    public boolean smoothTemp() { return mSmoothTemp; }
+    public int getNumHours() {
+      return mNumHours;
+    }
+    public boolean showTimeScale() {
+      return mShowTimeScale;
+    }
+    public boolean showTimeLines() {
+      return mShowTimeLines;
+    }
+
+    public static GraphSettings get(SharedPreferences pref, String prefix) {
+      GraphSettings gs = new GraphSettings();
+      gs.mPrefix = prefix;
+      gs.mAutoGraphSize = pref.getBoolean(prefix+"AutoGraph", true);
+      gs.mGraphWidth = pref.getInt(prefix + "GraphWidth", 40);
+      gs.mGraphHeight = pref.getInt(prefix + "GraphHeight", 40);
+      gs.mShowWatchGraph = pref.getBoolean(prefix+"IncludeWatchGraph", true);
+      gs.mShowBatteryGraph = pref.getBoolean(prefix+"IncludeBattery", true);
+      gs.mShowBatteryCurrentInstant = pref.getBoolean(prefix + "IncludeBatteryCurrentInstant", false);
+      gs.mSmoothBatteryCurrentInstant = pref.getBoolean(prefix + "SmoothBatteryCurrentInstant", false);
+      gs.mInvertBatteryCurrentInstant = pref.getBoolean(prefix + "InvertBatteryCurrentInstant", false);
+      gs.mShowBatteryCurrentAvg = pref.getBoolean(prefix + "IncludeBatteryCurrentAvg", false);
+      gs.mShowBatteryEnergy = pref.getBoolean(prefix+"IncludeBatteryEnergy", false);
+      gs.mShowTempGraph = pref.getBoolean(prefix+"IncludeTemp", false);
+      gs.mSmoothTemp = pref.getBoolean(prefix+"SmoothTemp", false);
+      gs.mTempCelsius = pref.getString(prefix+"TempUnits", "C").toLowerCase().equals("c");
+      gs.mNumHours = Integer.parseInt(pref.getString(prefix + "NumHours", Integer.toString(48)));
+      gs.mShowTimeScale = pref.getBoolean(prefix+"ShowTime", false);
+      gs.mShowTimeLines = pref.getBoolean(prefix+"ShowTimeLines", false);
+      return gs;
+    }
+
+    public void save(Context context) {
+      save(PreferenceManager.getDefaultSharedPreferences(context), mPrefix);
+    }
+
+    public void save(SharedPreferences pref, String prefix) {
+      pref.edit()
+        .putBoolean(prefix+"AutoGraph", mAutoGraphSize)
+        .putInt(prefix+"GraphWidth", mGraphWidth)
+        .putInt(prefix+"GraphHeight", mGraphHeight)
+        .putBoolean(prefix+"IncludeWatchGraph", mShowWatchGraph)
+        .putBoolean(pref + "IncludeBattery", mShowBatteryGraph)
+        .putBoolean(prefix + "IncludeBatteryCurrentInstant", mShowBatteryCurrentInstant)
+        .putBoolean(prefix + "IncludeBatteryCurrentAvg", mShowBatteryCurrentAvg)
+        .putBoolean(prefix + "IncludeBatteryEnergy", mShowBatteryEnergy)
+        .putBoolean(prefix+"IncludeTemp", mShowTempGraph)
+        .putString(prefix + "TempUnits", mTempCelsius ? "C" : "F")
+        .putString(prefix + "NumHours", Integer.toString(mNumHours))
+        .putBoolean(prefix+"ShowTime", mShowTimeScale)
+        .putBoolean(prefix+"ShowTimeLines", mShowTimeLines)
+        .apply();
+    }
+  }
+}
--- app/src/main/java/com/mcmhav/btcwidget/UpdatingWidget.java
+++ app/src/main/java/com/mcmhav/btcwidget/UpdatingWidget.java
@@ -23,6 +23,11 @@ public class UpdatingWidget extends AppWidgetProvider {
   private final int updateInterval = 60000;
   private static final String ACTION_SIMPLEAPPWIDGET = "ACTION_BROADCASTWIDGETSAMPLE";
 
+  // graph vars:
+  private float mPixelDensity;
+  private TreeMap<Integer, RemoteViews> mRemoteViews;
+  private Settings mSettings;
+
   static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 
     // Construct the RemoteViews object
@@ -70,6 +75,37 @@ public class UpdatingWidget extends AppWidgetProvider {
       Intent i = new Intent(context, UpdateService.class);
       context.startService(i);
     }
+
+    int[] appWidgetIds = null;
+    Bundle extras = intent.getExtras();
+    if (extras != null) {
+      appWidgetIds = extras.getIntArray(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+    }
+    if (appWidgetIds == null) {
+      appWidgetIds = AppWidgetManager.getInstance(context).getAppWidgetIds(
+          new ComponentName(context, BatteryGraphWidgetProvider.class)
+      );
+    }
+
+    mSettings = Settings.get(context);
+    if (mRemoteViews == null) {
+      mPixelDensity = context.getResources().getDisplayMetrics().density;
+      mRemoteViews = new TreeMap<>();
+      for (int appWidgetId : appWidgetIds) {
+        mRemoteViews.put(
+          appWidgetId,
+          new RemoteViews(context.getPackageName(), R.layout.widget)
+        );
+      }
+    }
+
+    if (appWidgetIds != null) {
+      for (int appWidgetId : appWidgetIds) {
+        AppWidgetManager.getInstance(context).updateAppWidget(
+            appWidgetId, mRemoteViews.get(appWidgetId)
+        );
+      }
+    }
   }
 
   @Override
@@ -100,5 +136,139 @@ public class UpdatingWidget extends AppWidgetProvider {
   public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
     super.onRestored(context, oldWidgetIds, newWidgetIds);
     LogH.d("onRestored");
+  }
+
+  // graph?:
+  private void refreshGraph(Context context, int[] appWidgetIds) {
+    for (int appWidgetId : appWidgetIds) {
+      Intent intent = new Intent(context, SettingsActivity.class);
+      intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+      intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+      PendingIntent pendingIntent = PendingIntent.getActivity(
+        context,
+        appWidgetId,
+        intent,
+        PendingIntent.FLAG_CANCEL_CURRENT
+      );
+
+      mRemoteViews
+        .get(appWidgetId)
+        .setOnClickPendingIntent(R.id.image, pendingIntent);
+
+      Settings.GraphSettings graphSettings = mSettings.getGraphSettings(appWidgetId);
+      int numMinutes = graphSettings.getNumHours() * 60;
+      Bitmap bmp = renderGraph(context, graphSettings, numMinutes);
+      mRemoteViews.get(appWidgetId).setImageViewBitmap(R.id.image, bmp);
+    }
+  }
+
+  private Bitmap renderGraph(Context context, Settings.GraphSettings graphSettings, int numMinutes) {
+    final int width = (int) (graphSettings.getGraphWidth() * mPixelDensity);
+    final int height = (int) (graphSettings.getGraphHeight() * mPixelDensity);
+    if (width == 0 || height == 0) {
+      return null;
+    }
+
+    Bitmap bmp = Bitmap.createBitmap(width, height, Config.ARGB_8888);
+    Canvas canvas = new Canvas(bmp);
+
+    int graphHeight = height;
+    if (graphSettings.showTimeScale()) {
+      graphHeight -= 10 * mPixelDensity;
+    }
+
+    List<BatteryStatus> batteryHistory = BatteryStatus.getHistory(
+      context,
+      0,
+      graphSettings.getNumHours()
+    );
+
+    int numGraphsShowing = 0;
+    List<GraphPoint> batteryChargePoints = null;
+
+    if (graphSettings.showBatteryGraph()) {
+      batteryChargePoints = renderChargeGraph(batteryHistory, numMinutes, width,
+          graphHeight, Color.GREEN);
+      numGraphsShowing++;
+    }
+
+    if (batteryChargePoints != null) {
+      drawGraphBackground(batteryChargePoints, canvas, width, graphHeight);
+    }
+
+    if (batteryChargePoints != null) {
+      drawGraphLine(batteryChargePoints, canvas);
+    }
+
+    String text = "";
+    if (graphSettings.showBatteryGraph() && batteryHistory.size() > 0) {
+      text = String.format(Locale.US, "%d%%", (int) (batteryHistory.get(0).getChargeFraction() * 100.0f));
+    }
+
+    Paint paint = new Paint();
+    paint.setAntiAlias(true);
+    paint.setTextSize(20.0f * mPixelDensity);
+    paint.setColor(Color.WHITE);
+    paint.setStyle(Style.FILL);
+    paint.setStrokeWidth(mPixelDensity);
+    float textWidth = paint.measureText(text);
+    canvas.drawText(text, width - textWidth - 4, graphHeight - 4, paint);
+
+    return bmp;
+  }
+
+  private void drawGraphBackground(List<GraphPoint> points, Canvas canvas, int width, int zeroValue) {
+    Path path = new Path();
+    path.moveTo(width, zeroValue);
+    for (GraphPoint pt : points) {
+      path.lineTo(pt.x, pt.y);
+    }
+    path.lineTo(0, zeroValue);
+    path.lineTo(width, zeroValue);
+    Paint paint = new Paint();
+    paint.setAntiAlias(true);
+    paint.setARGB(128, 255, 0, 0);
+    paint.setStyle(Style.FILL);
+    canvas.drawPath(path, paint);
+  }
+
+  private void drawGraphLine(List<GraphPoint> points, Canvas canvas) {
+    Paint paint = new Paint();
+    paint.setAntiAlias(true);
+    paint.setStyle(Style.STROKE);
+    paint.setAlpha(255);
+    paint.setStrokeWidth(4.0f);
+    Path path = null;
+    int colour = Color.BLACK;
+    for (GraphPoint pt : points) {
+      if (pt.colour != colour || path == null) {
+        if (path != null) {
+          path.lineTo(pt.x, pt.y);
+          paint.setColor(colour);
+          canvas.drawPath(path, paint);
+          colour = pt.colour;
+        }
+        path = new Path();
+        path.moveTo(pt.x, pt.y);
+      } else {
+        path.lineTo(pt.x, pt.y);
+      }
+    }
+    if (path != null) {
+      paint.setColor(colour);
+      canvas.drawPath(path, paint);
+    }
+  }
+
+  private static class GraphPoint {
+    public float x;
+    public float y;
+    public int colour;
+
+    public GraphPoint(float x, float y, int colour) {
+      this.x = x;
+      this.y = y;
+      this.colour = colour;
+    }
   }
 }
